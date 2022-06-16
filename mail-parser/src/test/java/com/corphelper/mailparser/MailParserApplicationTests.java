c5078a31134f1f455f471cd0351c00ad48c15f9d
maksim mayeuski <65280608+Maksim-maevsky@users.noreply.github.com>
2022-06-16 20:39:43 +0400

    refactored code

---
+++ mail-parser/src/test/java/com/corphelper/mailparser/MailParserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.corphelper.mailparser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MailParserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
