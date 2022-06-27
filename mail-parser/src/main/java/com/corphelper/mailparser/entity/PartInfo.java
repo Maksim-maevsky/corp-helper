b78cf4c656f9da16efb7eb99cc5a48e991ee7d82
maksim mayeuski <65280608+Maksim-maevsky@users.noreply.github.com>
2022-06-27 13:02:28 +0400

    refactored code

---
+++ mail-parser/src/main/java/com/corphelper/mailparser/entity/PartInfo.java
@@ -0,0 +1,11 @@
+package com.corphelper.mailparser.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PartInfo {
+}
